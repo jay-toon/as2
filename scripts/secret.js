f1b68bba7593eefa06a5e94a1624c3cfe6ce6773
jay-toon <jaytoon.dev@gmail.com>
2024-05-10 16:50:07 +0100

    This is improving.

--- scripts/secret.js
+++ scripts/secret.js
@@ -1,13 +1,20 @@
-import { rename } from 'fs';
+const soundtrack = './soundtrack/かめりあ.mp3';
 
 function renameMP3File(oldPath, newPath) {
-    rename(oldPath, newPath, (err) => {
-        if (err) {
-            console.error('Error renaming file:', err);
-        } else {
+    fetch(oldPath)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = newPath;   
+            a.click();
+            URL.revokeObjectURL(url);
             console.log('File renamed successfully!');
-        }
-    });
+        })
+        .catch(err => {
+            console.error('Error renaming file:', err);
+        });
 }
 
-renameMP3File('./soundtracks/かめりあ.mp3', './soundtracks/かめりあ - ???.mp3');
+renameMP3File(soundtrack, 'Camellia (かめりあ) - ???.mp3');
